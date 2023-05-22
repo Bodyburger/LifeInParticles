fbdb163d32fbc842652ede07bf0b83fcc16b574b
mratti.k@hotmail.fi <39917514+Bodyburger@users.noreply.github.com>
2023-05-22 13:43:07 +0300

    CopyPasted project to repository folder

---
+++ Source/LifeInParticles/LifeInParticlesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LifeInParticlesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIFEINPARTICLES_API ALifeInParticlesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
